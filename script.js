2f1478025787dd4dacccbbbfc41ee8de66cc4c37
gitgod997 <126290576+gitgod997@users.noreply.github.com>
2023-03-30 20:29:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USBTYPE C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K UltraHD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearet music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for Hours'
+    },
+];
+
+storeItems.forEach(function(item) {
+    if(item.inStock){
+      $('#productItems').append('<p class="inStock"><span>$'+item.price +'</span>'+ '<span class="center">' +item.name + '</span>' + '<span>' + item.details + '</span>' + '</p>')
+    }
+
+
+  });let darkToggle = $('#clickMe');
+  darkToggle.click(function() {
+   $('#contentContainer').toggleClass('darkMode')
+});
